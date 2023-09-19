3ff3123ba4e2b28569f30d1dcab66f7e0d9afbca
edem4963 <112492837+edem4963@users.noreply.github.com>
2023-09-18 23:31:38 -0400

    add data file to app component and create props in location component

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,17 @@
 
 import Header from './component/header'
 import Location from './component/location'
+import data from './data'
 import './App.css'
 
 function App() {
- 
+    
 
   return (
     <>
       <Header />
       <div className='main'>
-        <Location />
+        
       </div>
     </>
   )
--- src/component/location.jsx
+++ src/component/location.jsx
@@ -1,29 +1,26 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faLocationDot } from '@fortawesome/free-solid-svg-icons'
 
-export default function Location() {
+export default function Location({title, location, googleUrl, start, end, imageUrl, info }) {
     return(
         <div className='main--location'>
           <div className='main--location--image'>
             <img 
               className='location--image' 
-              src="https://source.unsplash.com/WLxQvbMyfas"  
-              alt="japan image" 
+              src={imageUrl}  
+              alt={`${location} image`} 
             />
           </div>
           <div className='main--location--description'>
             <div className='description--header'>
               <FontAwesomeIcon icon={faLocationDot} size="xs" style={{color: "#f55a5a",}} />
-              <h6 className='header--country'>JAPAN</h6>
-              <a href='#' className='google--link'>View on Google Maps</a>
+              <h6 className='header--country'>{location.toUppercase()}</h6>
+              <a href={googleUrl} className='google--link'>View on Google Maps</a>
             </div>
-            <h2 className="description--place">Mount Fuji</h2>
-            <p className='description--time'>12 Jan, 2021 - 24 Jan, 2021</p>
+            <h2 className="description--place">{title}</h2>
+            <p className='description--time'>{`${start} - ${end}`} </p>
             <p className='description--paragraph'>
-              Mount Fuji is the tallest mountain in Japan, 
-              standing at 3,776 meters (12,380 feet). 
-              Mount Fuji is the single most popular tourist site in Japan, 
-              for both Japanese and foreign tourists.
+              {info}
             </p>
           </div>
         </div>
